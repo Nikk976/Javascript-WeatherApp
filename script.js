b3ec32478880feab69b64bc5fbfc7d87c0ee06a5
Nikhil <nk4338996@gmail.com>
2024-03-19 14:30:33 +0530

    Added the top city weathers but dark mode is still in progress

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ const options = {
     "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
   },
 };
+
+const toFirstUpperCase = (str)=>{
+    return str.charAt(0).toUpperCase() + str.substr(1);
+}
 const getWeather = (city)=>{
-  fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`, options)
+  fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${toFirstUpperCase(city)}`, options)
     .then((response) => {
       return response.json();
     })
@@ -26,7 +30,7 @@ const getWeather = (city)=>{
       wind_degrees.innerHTML = response.wind_degrees;
       wind_speed.innerHTML = response.wind_speed;
       wind_speed2.innerHTML = response.wind_speed;
-      cityWeather.innerHTML = `${city} Weather`;
+      cityWeather.innerHTML = `${toFirstUpperCase(city)} Weather`;
     })
     .catch((err) => {
       console.log(err);
@@ -41,3 +45,90 @@ const getWeather = (city)=>{
     cityName.value = '';
   })
   getWeather('Delhi');
+
+
+  const getChandigarhWeather = ()=>{
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Chandigarh',options).then((response)=>{
+      return response.json();
+    }).then((response)=>{
+      let tr1 = document.getElementById('tr1');
+      let tableData = tr1.cells;
+      tableData = Array.from(tableData);
+      console.log(tableData);
+      tableData[1].innerHTML = response.cloud_pct
+      tableData[2].innerHTML = response.feels_like
+      tableData[3].innerHTML = response.humidity
+      tableData[4].innerHTML = response.max_temp
+      tableData[5].innerHTML = response.min_temp
+      tableData[6].innerHTML = response.sunrise
+      tableData[7].innerHTML = response.sunset
+      tableData[8].innerHTML = response.temp
+      tableData[9].innerHTML = response.wind_degrees
+      tableData[10].innerHTML = response.wind_speed
+    })
+  }
+  const getMumbaiWeather = ()=>{
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Mumbai',options).then((response)=>{
+      return response.json();
+    }).then((response)=>{
+      let tr2 = document.getElementById('tr2');
+      let tableData = tr2.cells;
+      tableData = Array.from(tableData);
+      console.log(tableData);
+      tableData[1].innerHTML = response.cloud_pct
+      tableData[2].innerHTML = response.feels_like
+      tableData[3].innerHTML = response.humidity
+      tableData[4].innerHTML = response.max_temp
+      tableData[5].innerHTML = response.min_temp
+      tableData[6].innerHTML = response.sunrise
+      tableData[7].innerHTML = response.sunset
+      tableData[8].innerHTML = response.temp
+      tableData[9].innerHTML = response.wind_degrees
+      tableData[10].innerHTML = response.wind_speed
+    })
+  }
+  const getChennaiWeather = ()=>{
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Chennai',options).then((response)=>{
+      return response.json();
+    }).then((response)=>{
+      let tr3 = document.getElementById('tr3');
+      let tableData = tr3.cells;
+      tableData = Array.from(tableData);
+      console.log(tableData);
+      tableData[1].innerHTML = response.cloud_pct
+      tableData[2].innerHTML = response.feels_like
+      tableData[3].innerHTML = response.humidity
+      tableData[4].innerHTML = response.max_temp
+      tableData[5].innerHTML = response.min_temp
+      tableData[6].innerHTML = response.sunrise
+      tableData[7].innerHTML = response.sunset
+      tableData[8].innerHTML = response.temp
+      tableData[9].innerHTML = response.wind_degrees
+      tableData[10].innerHTML = response.wind_speed
+    })
+  }
+  const getBengaluruWeather = ()=>{
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Bengaluru',options).then((response)=>{
+      return response.json();
+    }).then((response)=>{
+      let tr4 = document.getElementById('tr4');
+      let tableData = tr4.cells;
+      tableData = Array.from(tableData);
+      console.log(tableData);
+      tableData[1].innerHTML = response.cloud_pct
+      tableData[2].innerHTML = response.feels_like
+      tableData[3].innerHTML = response.humidity
+      tableData[4].innerHTML = response.max_temp
+      tableData[5].innerHTML = response.min_temp
+      tableData[6].innerHTML = response.sunrise
+      tableData[7].innerHTML = response.sunset
+      tableData[8].innerHTML = response.temp
+      tableData[9].innerHTML = response.wind_degrees
+      tableData[10].innerHTML = response.wind_speed
+    })
+  }
+
+  getChandigarhWeather();
+  getMumbaiWeather();
+  getChennaiWeather();
+  getBengaluruWeather();
